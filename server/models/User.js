88b5ce92c642596d5a5ee044848010efd08e375a
Shivamya94 <yadavshivam90440@gmail.com>
2025-04-28 18:16:26 +0530

    new files added

---
+++ server/models/User.js
@@ -0,0 +1,22 @@
+// server/models/User.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type:String,
+        required:true,
+        trim:true,
+  },
+  email: {
+    type:String,
+    required:true,
+    trim:true,
+  },
+  password: {
+    type:String,
+    required:true,
+  },
+  role: { type: String, enum: ['client', 'admin'], default: 'client' }
+});
+
+module.exports = mongoose.model('User', userSchema);
