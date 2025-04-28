6f1a983e87b276263fbe55e76dbcb961db9d7df0
Shivamya94 <yadavshivam90440@gmail.com>
2025-04-28 20:15:24 +0530

    new file added

---
+++ client/src/pages/Dashboardpage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DashboardPage() {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      {/* We will list feedback here */}
+    </div>
+  );
+}
+
+export default DashboardPage;
---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LoginPage() {
+  return (
+    <div>
+      <h1>Login to your account</h1>
+      {/* We will add login form here */}
+    </div>
+  );
+}
+
+export default LoginPage;
