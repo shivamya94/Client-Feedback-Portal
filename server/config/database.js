954ebfd9134e301acfb1e3ba211f3ec0ebae0225
Shivamya94 <yadavshivam90440@gmail.com>
2025-04-29 15:48:41 +0530

    new files added

--- server/config/database.js
+++ server/config/database.js
@@ -1,16 +1,35 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-require("dotenv").config();
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('✅ MongoDB connected');
+  } catch (err) {
+    console.error('❌ MongoDB connection failed:', err.message);
+    process.exit(1);
+  }
+};
 
-exports.connect = () => {
-    mongoose.connect(process.env.MONGODB_URL, {
+module.exports = connectDB;
 
-    })
-    .then( () => {console.log("DB connected Successfully")})
-    .catch( (err) => {
-        console.log("DB Connection issued");
-        console.error(err);
-        process.exit(1);
-    });
 
-}
+
+
+
+
+// const mongoose = require("mongoose");
+
+// require("dotenv").config();
+
+// exports.connect = () => {
+//     mongoose.connect(process.env.MONGODB_URL, {
+
+//     })
+//     .then( () => {console.log("DB connected Successfully")})
+//     .catch( (err) => {
+//         console.log("DB Connection issued");
+//         console.error(err);
+//         process.exit(1);
+//     });
+
+// }
