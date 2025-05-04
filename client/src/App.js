4ef18fb1f1808b665631be50c0392529ae263c4c
Shivamya94 <yadavshivam90440@gmail.com>
2025-05-05 02:01:56 +0530

    new file added

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,15 @@ import DashboardPage from './pages/DashboardPage';
 import Header from './components/Header';
 import RegisterPage from './pages/RegisterPage';
 import AdminPanel from './components/AdminPanel';
+import ApiTest from './components/ApiTest';
 
 function App() {
   return (
     <>
       <Header />
+      <ApiTest />
+
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/admin" element={<AdminPanel />} />
